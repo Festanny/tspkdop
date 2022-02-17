8790da56ef55d71afa3df0c3fda827232fe480cf
Festanny <junior-format30@mail.ru>
2022-02-17 21:44:00 +0400

    add

--- script/script.js
+++ script/script.js
@@ -1,9 +1,49 @@
 $(document).ready(function(){
-  $('.carousel-autoplay').slick({
-	  dots: true,
-	  slidesToShow: 3,
-	  slidesToScroll: 1,
-	  autoplay: false,
-	  autoplaySpeed: 5000,
-	});
+	if ($(window).width() >= 573) {
+		$('.carousel-autoplay').slick({
+			dots: true,
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			autoplay: false,
+			autoplaySpeed: 5000,
+		  });
+	}
+	else if ($(window).width() < 573) {
+		$('.carousel-autoplay').slick({
+			dots: true,
+			slidesToShow: 2,
+			slidesToScroll: 1,
+			autoplay: false,
+			autoplaySpeed: 5000,
+		  });
+	}
 });
+
+
+
+// $(document).on('ready', function() {
+// 	if ($(window).width() >= 769) {
+// 		$(".regular").slick({
+// 			dots: true,
+// 			infinite: true,
+// 			slidesToShow: 3,
+// 			slidesToScroll: 3
+// 		});
+// 	}
+// 	else if ($(window).width() > 480 && $(window).width() < 769) {
+// 		$(".regular").slick({
+// 			dots: true,
+// 			infinite: true,
+// 			slidesToShow: 2,
+// 			slidesToScroll: 2
+// 		});
+// 	}
+// 	else {
+// 		$(".regular").slick({
+// 			dots: true,
+// 			infinite: true,
+// 			slidesToShow: 1,
+// 			slidesToScroll: 1
+// 		});
+// 	}
+// });
