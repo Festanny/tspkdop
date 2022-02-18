230cc97a9267037e450f05dfd94fc07aa62ca867
Festanny <junior-format30@mail.ru>
2022-02-18 19:00:32 +0400

    add 400px

--- script/script.js
+++ script/script.js
@@ -5,45 +5,25 @@ $(document).ready(function(){
 			slidesToShow: 3,
 			slidesToScroll: 1,
 			autoplay: false,
-			autoplaySpeed: 5000,
+			autoplaySpeed: 5000
 		  });
 	}
-	else if ($(window).width() < 573) {
+	else if ($(window).width() > 430 && $(window).width() < 573) {
 		$('.carousel-autoplay').slick({
 			dots: true,
 			slidesToShow: 2,
 			slidesToScroll: 1,
 			autoplay: false,
-			autoplaySpeed: 5000,
+			autoplaySpeed: 5000
+		  });
+	}
+	else {
+		$('.carousel-autoplay').slick({
+			dots: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			autoplay: false,
+			autoplaySpeed: 5000
 		  });
 	}
 });
-
-
-
-// $(document).on('ready', function() {
-// 	if ($(window).width() >= 769) {
-// 		$(".regular").slick({
-// 			dots: true,
-// 			infinite: true,
-// 			slidesToShow: 3,
-// 			slidesToScroll: 3
-// 		});
-// 	}
-// 	else if ($(window).width() > 480 && $(window).width() < 769) {
-// 		$(".regular").slick({
-// 			dots: true,
-// 			infinite: true,
-// 			slidesToShow: 2,
-// 			slidesToScroll: 2
-// 		});
-// 	}
-// 	else {
-// 		$(".regular").slick({
-// 			dots: true,
-// 			infinite: true,
-// 			slidesToShow: 1,
-// 			slidesToScroll: 1
-// 		});
-// 	}
-// });
