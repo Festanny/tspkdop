7948a5269c620b32445059e44705d1a2ad36a2a3
Festanny <junior-format30@mail.ru>
2022-03-27 20:54:13 +0400

    upd carousel

--- script/script.js
+++ script/script.js
@@ -1,29 +1,23 @@
 $(document).ready(function(){
-	if ($(window).width() >= 573) {
-		$('.carousel-autoplay').slick({
-			dots: true,
-			slidesToShow: 3,
-			slidesToScroll: 1,
-			autoplay: true,
-			autoplaySpeed: 5000
-		  });
-	}
-	else if ($(window).width() > 430 && $(window).width() < 573) {
-		$('.carousel-autoplay').slick({
-			dots: true,
-			slidesToShow: 2,
-			slidesToScroll: 1,
-			autoplay: true,
-			autoplaySpeed: 5000
-		  });
-	}
-	else {
-		$('.carousel-autoplay').slick({
-			dots: true,
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			autoplay: true,
-			autoplaySpeed: 5000
-		  });
-	}
+	$('.carousel-autoplay').slick({
+		dots: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		responsive: [
+			{
+			  breakpoint: 573,
+			  settings: {
+				slidesToShow: 2
+			  }
+			},
+			{
+			  breakpoint: 431,
+			  settings: {
+				slidesToShow: 1
+			  }
+			}
+		]
+	});
 });
